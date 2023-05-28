9ce3f57a8bbfb90a3a2cf7c6c6e26bdfaf4a5697
iam-deepakvel <deepakvel82@gmail.com>
2023-05-28 13:55:45 +0530

    feat: socket.io integration

--- module.ts
+++ module.ts
@@ -32,9 +32,49 @@ export class AppModule {
         throw new Error("MONGO_URI is required");
       }
       await connectDB(process.env.MONGO_URI);
-      this.app.listen(PORT, () =>
+      const server = this.app.listen(PORT, () =>
         console.log(`Server is listening on port ${PORT}...`)
       );
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const io = require("socket.io")(server, {
+        pingTimeOut: 60000,
+        cors: {
+          origin: "*",
+        },
+      });
+      io.on("connection", (socket: any) => {
+        console.log("conntected to socket.io");
+        socket.on("setup", (userData: any) => {
+          socket.join(userData._id);
+          socket.emit("connected");
+        });
+
+        socket.on("join chat", (room: any) => {
+          socket.join(room);
+          console.log("User Joined Room: " + room);
+        });
+
+        socket.on("typing", (room: any) => socket.in(room).emit("typing"));
+        socket.on("stop typing", (room: any) =>
+          socket.in(room).emit("stop typing")
+        );
+
+        socket.on("new message", (newMessageReceived: any) => {
+          const chat: any = newMessageReceived.chat;
+          if (!chat.users) {
+            return console.log("Chat.users not defined");
+          }
+          chat.users.forEach((user: any) => {
+            if (user._id == newMessageReceived.sender._id) return;
+            socket.in(user._id).emit("message received", newMessageReceived);
+          });
+
+          socket.off("setup", (userData: any) => {
+            console.log("User Disconnected");
+            socket.leave(userData._id);
+          });
+        });
+      });
     } catch (error) {
       console.log(error);
     }
